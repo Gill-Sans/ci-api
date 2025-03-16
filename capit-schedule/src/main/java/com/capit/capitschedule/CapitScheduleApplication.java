caadef3d8dbdcd0bfbbb3ca96bcaef8c058d7279
gill-sans <mertensgill@outlook.com>
2025-03-16 15:26:55 +0100

    Initial commit

---
+++ capit-schedule/src/main/java/com/capit/capitschedule/CapitScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.capit.capitschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CapitScheduleApplication.class, args);
+    }
+
+}
---
+++ capit-schedule/src/test/java/com/capit/capitschedule/CapitScheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capit.capitschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitScheduleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
