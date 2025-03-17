bb0405a00d9a330e3c8d03586cc59c48b4ffeca4
gill-sans <mertensgill@outlook.com>
2025-03-17 12:07:50 +0100

    Feat: add CheckinDetails entity and projection for handling check-in events

---
+++ capit-interactions/src/main/java/com/capit/capitinteractions/domain/impl/checkin/projections/CheckinDetails.java
@@ -0,0 +1,24 @@
+package com.capit.capitinteractions.domain.impl.checkin.projections;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CheckinDetails {
+    @Id
+    private UUID id;
+    private UUID userId;
+    private UUID sessionId;
+}
