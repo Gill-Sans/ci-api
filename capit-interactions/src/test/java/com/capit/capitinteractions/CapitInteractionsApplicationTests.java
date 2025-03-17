45fd9cb30027b1c1df4e01ce62328585426fe9f0
gill-sans <mertensgill@outlook.com>
2025-03-17 09:26:36 +0100

    Chore: add capit-interactions module to pom.xml

---
+++ capit-interactions/src/test/java/com/capit/capitinteractions/CapitInteractionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capit.capitinteractions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitInteractionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
