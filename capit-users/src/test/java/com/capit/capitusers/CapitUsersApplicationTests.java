91854a7711d66c8ed553801e5ce55f148659a65d
gill-sans <mertensgill@outlook.com>
2025-03-21 10:42:47 +0100

    Chore: initialize capit-users project with Maven setup and basic application structure

---
+++ capit-users/src/test/java/com/capit/capitusers/CapitUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capit.capitusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
