318672b400d7fd9d19fd1561d055227e208ec553
gill-sans <mertensgill@outlook.com>
2025-03-24 00:53:29 +0100

    Feat: add routing and security configuration for the gateway service

---
+++ capit-gateway/src/main/java/com/capit/capitgateway/routes/Routes.java
@@ -0,0 +1,34 @@
+package com.capit.capitgateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+
+    @Bean
+    public RouterFunction<ServerResponse> InteractionServiceRoute() {
+        return GatewayRouterFunctions.route("interaction_service")
+                .route(RequestPredicates.path("/api/interaction"), HandlerFunctions.http("http://localhost:8081"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> ScheduleServiceRoute() {
+        return GatewayRouterFunctions.route("schedule_service")
+                .route(RequestPredicates.path("/api/schedule"), HandlerFunctions.http("http://localhost:8082"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> UserServiceRoute() {
+        return GatewayRouterFunctions.route("user_service")
+                .route(RequestPredicates.path("/api/user"), HandlerFunctions.http("http://localhost:8080"))
+                .build();
+    }
+}
---
+++ capit-gateway/src/test/java/com/capit/capitgateway/CapitGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capit.capitgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
