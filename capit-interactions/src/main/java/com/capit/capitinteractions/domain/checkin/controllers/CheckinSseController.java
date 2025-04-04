a03bf2a2c1b62b1d12caefd9fd88d2775d0bd2b5
gill-sans <mertensgill@outlook.com>
2025-04-04 09:49:04 +0200

    Refactor: remove unused CheckinService dependency from CheckinSseController

--- capit-interactions/src/main/java/com/capit/capitinteractions/domain/checkin/controllers/CheckinSseController.java
+++ capit-interactions/src/main/java/com/capit/capitinteractions/domain/checkin/controllers/CheckinSseController.java
@@ -1,7 +1,6 @@
 package com.capit.capitinteractions.domain.checkin.controllers;
 
 import com.capit.capitinteractions.domain.checkin.events.CheckinEvent;
-import com.capit.capitinteractions.domain.checkin.service.CheckinService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.event.EventListener;
 import org.springframework.http.MediaType;
@@ -11,15 +10,12 @@ import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import java.util.concurrent.CopyOnWriteArrayList;
 
 @RestController
 @RequiredArgsConstructor
 public class CheckinSseController {
-
-    private final CheckinService checkinService;
+    
     private final List<SseEmitter> emitters = new CopyOnWriteArrayList<>();
 
     @GetMapping(value = "/api/interaction/check-ins/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
