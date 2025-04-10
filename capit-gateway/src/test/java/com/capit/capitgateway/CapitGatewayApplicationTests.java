3e8d69f3b99f94b15dc7be831f08cccc30771993
gill-sans <mertensgill@outlook.com>
2025-04-11 00:59:43 +0200

    Feat: add MockitoBean for JwtDecoder in CapitGatewayApplicationTests

--- capit-gateway/src/test/java/com/capit/capitgateway/CapitGatewayApplicationTests.java
+++ capit-gateway/src/test/java/com/capit/capitgateway/CapitGatewayApplicationTests.java
@@ -2,9 +2,14 @@ package com.capit.capitgateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 
 @SpringBootTest
 class CapitGatewayApplicationTests {
+
+	@MockitoBean
+	private JwtDecoder jwtDecoder;
 
 	@Test
 	void contextLoads() {
