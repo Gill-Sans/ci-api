75000fad0945ffe918a3e124067d5872b8f3daa0
Gill Mertens <mertensgill@outlook.com>
2025-04-25 06:39:39 +0200

    Feat: configure Kafka settings in application-local.yml and update dependencies for WebFlux and Reactor Kafka support

--- capit-interactions/src/main/java/com/capit/capitinteractions/config/WebSecurityConfig.java
+++ capit-interactions/src/main/java/com/capit/capitinteractions/config/WebSecurityConfig.java
@@ -1,30 +1,30 @@
-package com.capit.capitinteractions.config;
-
-import com.capit.config.GatewayFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-
-@Configuration
-@EnableWebSecurity
-public class WebSecurityConfig {
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(AbstractHttpConfigurer::disable)
-                .cors(AbstractHttpConfigurer::disable)
-                .addFilterBefore(gatewayFilter(), BasicAuthenticationFilter.class)
-                .authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
-        return http.build();
-    }
-
-    @Bean
-    public GatewayFilter gatewayFilter() {
-        return new GatewayFilter();
-    }
-}
+//package com.capit.capitinteractions.config;
+//
+//import com.capit.config.GatewayFilter;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+//import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class WebSecurityConfig {
+//
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//        http
+//                .csrf(AbstractHttpConfigurer::disable)
+//                .cors(AbstractHttpConfigurer::disable)
+//                .addFilterBefore(gatewayFilter(), BasicAuthenticationFilter.class)
+//                .authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
+//        return http.build();
+//    }
+//
+//    @Bean
+//    public GatewayFilter gatewayFilter() {
+//        return new GatewayFilter();
+//    }
+//}
